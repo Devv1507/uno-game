13cc07d56b7014ca892556cb01cc7fd88e56b19f
Devv1507 <davide.valencia.v@gmail.com>
2025-04-19 20:25:39 -0500

    Added the firm to follow in model and controller layers to avoid conflicts

---
+++ src/main/java/univalle/tedesoft/uno/model/GameState.java
@@ -0,0 +1,8 @@
+package univalle.tedesoft.uno.model;
+
+/**
+ * TODO: esta debe ser la clase principal que representa el estado del juego
+ * Debe orquestar los turnos, el mazo, los jugadores y la pila de descarte.
+ */
+public class GameState extends IGameState {
+}
