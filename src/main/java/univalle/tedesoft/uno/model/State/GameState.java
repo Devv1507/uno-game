c8e9a763b4f1489913767417bb71955669bca874
Devv1507 <davide.valencia.v@gmail.com>
2025-04-26 21:30:23 -0500

    Formated file

--- src/main/java/univalle/tedesoft/uno/model/State/GameState.java
+++ src/main/java/univalle/tedesoft/uno/model/State/GameState.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 /**
  * TODO: esta debe ser la clase principal que representa el estado del juego
  * Debe orquestar los turnos, el mazo, los jugadores y la pila de descarte.
+ *
  * @author David Esteban Valencia
  * @author Santiago David Guerrero
  * @author Juan Pablo Escamilla
@@ -72,9 +73,11 @@ public class GameState implements IGameState {
 
     /**
      * Se llama cada vez que el turno pasa de un jugador a otro.(Funcion que pertenece al controlador de los eventos)
+     *
      * @param currentPlayer El jugador que ahora tiene el turno.
      */
-    public void onTurnChanged(Player currentPlayer) {}
+    public void onTurnChanged(Player currentPlayer) {
+    }
 
     /**
      * Se llama al inicio, para repartir la primera mano de cartas a ambos jugadores.
@@ -92,30 +95,38 @@ public class GameState implements IGameState {
 
     /**
      * Se llama cuando un jugador ha jugado exitosamente una carta.
+     *
      * @param player El jugador que realizo la jugada.
-     * @param card La carta especifica que fue jugada y ahora está en la cima.
+     * @param card   La carta especifica que fue jugada y ahora está en la cima.
      */
     public void onCardPlayed(Player player, Card card) {
         player.playCard(card);
         discardStack.discard(card);
     }
+
     /**
      * Se llama cuando un jugador toma una carta del mazo (ya sea por accion voluntaria
      * o por efecto de una carta especial, o penalización).
+     *
      * @param player    El jugador que tomo la carta.
      * @param drawnCard La carta especifica que fue tomado.
      */
-    public void onPlayerDrewCard(Player player, Card drawnCard){
+    public void onPlayerDrewCard(Player player, Card drawnCard) {
     }
+
     /**
      * Se llama cuando la mano de un jugador ha cambiado (se añadieron o quitaron cartas).
      * Esto puede ser resultado de jugar, tomar cartas, o ser penalizado.
+     *
      * @param player El jugador cuya mano necesita ser actualizada en la vista.
      */
-    public void onHandChanged(Player player) {}
+    public void onHandChanged(Player player) {
+    }
+
     /**
      * Se llama específicamente cuando un jugador es forzado a tomar cartas
      * debido a un efecto (+2, +4) o una penalización (no decir UNO).
+     *
      * @param player        El jugador que va a tomar cartas.
      * @param numberOfCards La cantidad de cartas que será forzado a tomar.
      */
@@ -141,40 +152,56 @@ public class GameState implements IGameState {
 
     /**
      * Se llama cuando un jugador es saltado (Skip, Reverse, o +2/+4).
+     *
      * @param skippedPlayer El jugador cuyo turno ha sido saltado.
      */
     public void onPlayerSkipped(Player skippedPlayer) {
     }
+
     /**
      * Se llama tras jugar un comodín (Change Color),
      * indicando que el jugador que la jugó debe elegir un color.
+     *
      * @param player El jugador que debe realizar la elección.
      */
-    public void onMustChooseColor(Player player) {}
+    public void onMustChooseColor(Player player) {
+    }
+
     /**
      * Se llama después de que un jugador ha elegido un color para un comodín,
      * o al inicio si la primera carta establece un color.
+     *
      * @param color El color que ahora está activo en el juego.
      */
-    public void onColorChosen(Color color) {}
+    public void onColorChosen(Color color) {
+    }
+
     /**
      * Se llama cuando el estado de "tener una sola carta" de un jugador cambia.
+     *
      * @param player     El jugador afectado.
      * @param hasOneCard true si el jugador AHORA tiene exactamente una carta,
      *                   false si tenía una y ahora tiene más (ej. por robar penalización).
      */
-    public void onUnoStateChanged(Player player, boolean hasOneCard) {}
+    public void onUnoStateChanged(Player player, boolean hasOneCard) {
+    }
+
     /**
      * Se llama cuando un jugador (humano o máquina) intenta declarar "UNO".
+     *
      * @param player  El jugador que declaró UNO.
      * @param success true si la declaración fue válida, false si fue inválida (tenía más de 1 carta).
      */
-    public void onUnoDeclared(Player player, boolean success){}
+    public void onUnoDeclared(Player player, boolean success) {
+    }
+
     /**
      * Se llama cuando el juego termina porque un jugador se ha quedado sin cartas.
+     *
      * @param winner El jugador que ganó la partida.
      */
-    public void onGameOver(Player winner) {}
+    public void onGameOver(Player winner) {
+    }
 
     /**
      * Recicla las cartas de la pila de descarte de vuelta al mazo principal
