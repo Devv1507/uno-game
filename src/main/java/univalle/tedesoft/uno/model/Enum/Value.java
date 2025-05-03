5ef9f85f633b89bc2a72d697219658d107f38b97
Devv1507 <davide.valencia.v@gmail.com>
2025-05-03 16:17:41 -0500

    Updated branch based on main

--- src/main/java/univalle/tedesoft/uno/model/Enum/Value.java
+++ src/main/java/univalle/tedesoft/uno/model/Enum/Value.java
@@ -10,7 +10,6 @@ package univalle.tedesoft.uno.model.Enum;
 public enum Value {
     ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
     SKIP,
-    REVERSE,
     DRAW_TWO,
     WILD,
     WILD_DRAW_FOUR
