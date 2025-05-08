cbe6d84f26b93538d5f9cb08e9011bcc35ffe564
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2025-05-08 17:30:11 -0500

    added information for the javadoc

--- src/main/java/univalle/tedesoft/uno/model/Players/HumanPlayer.java
+++ src/main/java/univalle/tedesoft/uno/model/Players/HumanPlayer.java
@@ -1,6 +1,17 @@
 package univalle.tedesoft.uno.model.Players;
 
+/**
+ * Clase que representa al jugador humano.
+ * @author David Esteban Valencia
+ * @author Santiago David Guerrero
+ * @author Juan Pablo Escamilla
+ */
 public class HumanPlayer extends Player {
+    /**
+     * Constructor para la clase HumanPlayer, recibe solo el nombre del jugador.
+     *
+     * @param playerName nombre del jugador.
+     */
     public HumanPlayer(String playerName) {
         this.name = playerName;
     }
