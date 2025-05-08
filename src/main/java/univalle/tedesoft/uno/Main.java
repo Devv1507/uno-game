407cb110a47bb3fe30f3da14ce3bc1509831c9e7
Juan Pablo ESCAMILLA MONTILLA <juanpabloeskmilla@gmail.com>
2025-05-07 20:39:27 -0500

    Created WelcomeView

--- src/main/java/univalle/tedesoft/uno/Main.java
+++ src/main/java/univalle/tedesoft/uno/Main.java
@@ -1,16 +1,20 @@
 package univalle.tedesoft.uno;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-import univalle.tedesoft.uno.view.GameView;
 
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        GameView welcomeView = GameView.getInstance();
-        welcomeView.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("UNO! - Bienvenida");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
